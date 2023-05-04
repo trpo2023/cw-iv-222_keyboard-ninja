03ffd8a06983007540d652f1e02b6ffbb0705fd2
Kirill Ogarkov <Klipo07@example.com>
2023-05-04 16:28:23 +0700

    Update basics file

--- new_interface.c
+++ new_interface.c
@@ -1,103 +1,145 @@
-#include <gtk/gtk.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <ctype.h>
+#include <ncurses.h>
+#include <math.h>
 
-void on_difficulty_changed(GtkComboBox *widget, gpointer data)
-{
-    // Получаем выбранное значение сложности
-    gchar *difficulty = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(widget));
+#define MAX_WORD_LENGTH 50
+#define MAX_NUM_WORDS 10000
 
-    // Используем выбранную сложность для установки уровня сложности
-    // ...
+void play_game(char words[MAX_NUM_WORDS][MAX_WORD_LENGTH], int min_len, int max_len, int num_rounds) {
+    char input[MAX_WORD_LENGTH];
+    int i, len, score = 0, used[MAX_NUM_WORDS] = {0}, word_index;
+    time_t start_time, end_time;
+    float total_time = 0;
+    int correct_words = 0;
 
-    g_free(difficulty);
+    for (i = 0; i < num_rounds; i++) {
+        start_time = time(NULL);
+        do {
+            word_index = rand() % MAX_NUM_WORDS;
+            len = strlen(words[word_index]);
+        } while (used[word_index] || len < min_len || len > max_len);
+        used[word_index] = 1;
+        clear();
+        printw("%s\n", words[word_index]);
+        refresh();
+        int ch, pos = 0;
+        while (1) {
+            ch = getch();
+            if (ch == KEY_BACKSPACE || ch == 127) {
+                if (pos > 0) {
+                    pos--;
+                    input[pos] = '\0';
+                }
+            } else if (ch == KEY_ENTER || ch == '\n') {  // If user pressed Enter
+                if (strcasecmp(input, words[word_index]) == 0) {
+                    score++;
+                    correct_words++;
+                } else {
+                    printw("Incorrect!\n");
+                }
+                break;
+            } else if (isalpha(ch)) {
+                if (pos < MAX_WORD_LENGTH - 1) {
+                    input[pos] = toupper(ch);
+                    pos++;
+                    input[pos] = '\0';
+                }
+            }
+            clear();
+            printw("%s\n%s", words[word_index], input);
+            refresh();
+        }
+        end_time = time(NULL);
+        total_time += difftime(end_time, start_time);
+    }
+
+    // Calculate accuracy
+    float accuracy = ((float) score / num_rounds) * 100;
+    float f_score = (float) score / (total_time / 60);
+
+    // Display results
+    clear();
+    printw("Accuracy: %.2f%%\n", accuracy);
+    printw("Time taken: %.0f:%02.0f\n", total_time / 60, fmod(total_time, 60));
+    printw("%.2f words per minute\n", f_score);
+    printw("Correctly entered words: %d out of %d\n", correct_words, num_rounds);
+    printw("End time: %s", ctime(&end_time)); // вывод времени окончания игры
+
+    //highscore.txt
+    // Read high score from file
+    int high_score = 0;
+    FILE *fp = fopen("highscore.txt", "r");
+    if (fp != NULL) {
+    fscanf(fp, "%d", &high_score);
+    fclose(fp);
+    }
+    // Check if current score is higher than high score
+    if (score > high_score) {
+    // Update high score
+    high_score = score;
+
+    // Write new high score to file
+    fp = fopen("highscore.txt", "w");
+    if (fp != NULL) {
+        fprintf(fp, "%d", high_score);
+        fclose(fp);
+    }
+
+    // Display message about new high score
+    printw("New high score: %d!\n", high_score);
+    } else {
+    // Display message about current high score
+    printw("Current high score: %d\n", high_score);
+    }
+
+    // Wait for user input before exiting
+    printw("\nPress any key to exit...");
+    getch();
 }
 
-void on_color_button_clicked(GtkButton *button, gpointer data)
-{
-    // Получаем выбранный цвет
-    gchar *color = gtk_button_get_label(GTK_BUTTON(button));
+int main() {
+    srand(time(NULL));
+    char words[MAX_NUM_WORDS][MAX_WORD_LENGTH];
+    int num_words = 0;
+    int min_len = 4, max_len = 8, num_rounds = 10;
+    // Open file containing words
+    FILE *fp = fopen("words.txt", "r");
+    if (fp == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
 
-    // Используем выбранный цвет для изменения цвета интерфейса
-    // ...
+    // Read words from file
+    char word[MAX_WORD_LENGTH];
+    while (fgets(word, MAX_WORD_LENGTH, fp)) {
+        // Remove trailing newline character
+        word[strcspn(word, "\n")] = '\0';
 
-    g_free(color);
-}
+        // Add word to array if it is the right length
+        int len = strlen(word);
+        if (len >= min_len && len <= max_len && num_words < MAX_NUM_WORDS) {
+            strcpy(words[num_words], word);
+            num_words++;
+        }
+    }
 
-int main(int argc, char *argv[])
-{
-    GtkWidget *window;
-    GtkWidget *vbox;
-    GtkWidget *hbox;
-    GtkWidget *difficulty_label;
-    GtkWidget *difficulty_combo;
-    GtkWidget *color_label;
-    GtkWidget *color_red_button;
-    GtkWidget *color_green_button;
-    GtkWidget *color_blue_button;
+    // Close file
+    fclose(fp);
 
-    gtk_init(&argc, &argv);
+    // Initialize ncurses
+    initscr();
+    cbreak();
+    noecho();
 
-    // Создаем окно
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Keyboard Trainer");
-    gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    // Play game
+    play_game(words, min_len, max_len, num_rounds);
 
-    // Создаем вертикальный контейнер
-    vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
-    gtk_container_add(GTK_CONTAINER(window), vbox);
-
-    // Создаем горизонтальный контейнер для сложности
-    hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
-    gtk_box_pack_start(GTK_BOX(vbox), hbox, FALSE, FALSE, 0);
-
-    // Создаем метку для сложности
-    difficulty_label = gtk_label_new("Difficulty:");
-    gtk_box_pack_start(GTK_BOX(hbox), difficulty_label, FALSE, FALSE, 0);
-
-    // Создаем выпадающий список для сложности
-    difficulty_combo = gtk_combo_box_text_new();
-    // Добавляем варианты сложности в выпадающий список
-    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(difficulty_combo), "Easy");
-    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(difficulty_combo), "Medium");
-    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(difficulty_combo), "Hard");
-
-    // Устанавливаем значение по умолчанию
-    gtk_combo_box_set_active(GTK_COMBO_BOX(difficulty_combo), 0);
-
-    // Добавляем обработчик события выбора сложности
-    g_signal_connect(difficulty_combo, "changed", G_CALLBACK(on_difficulty_changed), NULL);
-
-    // Добавляем выпадающий список в горизонтальный контейнер
-    gtk_box_pack_start(GTK_BOX(hbox), difficulty_combo, FALSE, FALSE, 0);
-
-    // Создаем горизонтальный контейнер для цвета
-    hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
-    gtk_box_pack_start(GTK_BOX(vbox), hbox, FALSE, FALSE, 0);
-
-    // Создаем метку для цвета
-    color_label = gtk_label_new("Color:");
-    gtk_box_pack_start(GTK_BOX(hbox), color_label, FALSE, FALSE, 0);
-
-    // Создаем кнопки для выбора цвета
-    color_red_button = gtk_button_new_with_label("Red");
-    color_green_button = gtk_button_new_with_label("Green");
-    color_blue_button = gtk_button_new_with_label("Blue");
-
-    // Добавляем обработчики события клика по кнопкам цвета
-    g_signal_connect(color_red_button, "clicked", G_CALLBACK(on_color_button_clicked), NULL);
-    g_signal_connect(color_green_button, "clicked", G_CALLBACK(on_color_button_clicked), NULL);
-    g_signal_connect(color_blue_button, "clicked", G_CALLBACK(on_color_button_clicked), NULL);
-
-    // Добавляем кнопки цвета в горизонтальный контейнер
-    gtk_box_pack_start(GTK_BOX(hbox), color_red_button, FALSE, FALSE, 0);
-    gtk_box_pack_start(GTK_BOX(hbox), color_green_button, FALSE, FALSE, 0);
-    gtk_box_pack_start(GTK_BOX(hbox), color_blue_button, FALSE, FALSE, 0);
-
-    // Отображаем окно
-    gtk_widget_show_all(window);
-
-    // Запускаем главный цикл обработки событий
-    gtk_main();
+    // Clean up ncurses
+    endwin();
 
     return 0;
 }
