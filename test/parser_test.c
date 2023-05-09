b3a250ff5d76055e956e1a6eec9b9af37fdaee57
Kirill Ogarkov <Klipo07@example.com>
2023-05-09 16:17:04 +0700

    Update files

--- test/parser_test.c
+++ test/parser_test.c
@@ -5,7 +5,8 @@
 #include <ctest.h>
 
 // Функция для проверки, что два слова равны
-CTEST(compare_words, test_equal_words) {
+CTEST(compare_words, test_equal_words)
+{
     char word1[] = "apple";
     char word2[] = "apple";
 
@@ -15,7 +16,8 @@ CTEST(compare_words, test_equal_words) {
 }
 
 // Функция для проверки, что два слова не равны
-CTEST(compare_words, test_unequal_words) {
+CTEST(compare_words, test_unequal_words)
+{
     char word1[] = "apple";
     char word2[] = "banana";
 
@@ -25,14 +27,33 @@ CTEST(compare_words, test_unequal_words) {
 }
 
 // Пример теста для функции play_game
-CTEST(play_game, test_play_game) {
+CTEST(play_game, test_play_game)
+{
     char words[MAX_NUM_WORDS][MAX_WORD_LENGTH] = {"apple", "banana", "orange"};
     int min_len = 3;
     int max_len = 6;
     int num_rounds = 2;
 
+    // Перенаправляем вывод в буфер, чтобы проверить результаты
+    FILE* stream = freopen("output.txt", "w", stdout);
+
+    // Вызываем функцию play_game
     play_game(words, min_len, max_len, num_rounds);
 
-    // Добавьте здесь проверки на ожидаемые результаты игры
-    // Используйте макросы ASSERT_* для проверки результатов
+    // Закрываем поток вывода
+    fclose(stream);
+
+    // Открываем файл для чтения и считываем результаты игры
+    FILE* result_file = fopen("output.txt", "r");
+    char buffer[100];
+    fgets(buffer, sizeof(buffer), result_file);
+
+    // Добавьте проверки на ожидаемые результаты
+    ASSERT_STR(
+            "Accuracy:",
+            buffer); // Проверяем, что строка начинается с "Accuracy:"
+    // Добавьте другие проверки для остальных ожидаемых результатов
+
+    // Закрываем файл
+    fclose(result_file);
 }
